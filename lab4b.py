3ca2817a2129cf75710ba30e11b6545056c1c177
Ma Rabe <mrabe@myseneca.ca>
2024-10-03 20:27:42 -0400

    COMPLETED LAB4.

---
+++ lab4b.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def join_lists(l1, l2):
+    # Join two lists and return a list containing every value from both
+    return list(set(l1) | set(l2))  # Using set to eliminate duplicates
+
+def match_lists(l1, l2):
+    # Return a list that contains all values found in both lists
+    return list(set(l1) & set(l2))  # Intersection of two sets
+
+def diff_lists(l1, l2):
+    # Return a list that contains all different values not shared between the lists
+    return list(set(l1) ^ set(l2))  # Symmetric difference
+
+if __name__ == '__main__':
+    list1 = list(range(1, 10))  # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    list2 = list(range(5, 15))   # [5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    print('list1: ', list1)
+    print('list2: ', list2)
+    print('join: ', join_lists(list1, list2))
+    print('match: ', match_lists(list1, list2))
+    print('diff: ', diff_lists(list1, list2))
